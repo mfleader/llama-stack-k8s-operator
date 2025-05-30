ad16fdfc3e0931539df5c5cf8ef062ddf075c41c
Matthew F Leader <mleader@redhat.com>
2025-05-30 18:41:30 -0400

    improve code comments
    Signed-off-by: Matthew F Leader <mleader@redhat.com>

--- pkg/deploy/kustomize_test.go
+++ pkg/deploy/kustomize_test.go
@@ -11,10 +11,13 @@ import (
 	"sigs.k8s.io/kustomize/kyaml/filesys"
 )
 
-func TestRenderKustomize_InMemory(t *testing.T) {
+func TestRenderKustomize(t *testing.T) {
+	// Use Kustomize in memory filesystem, so that we can define
+	// our test fixtures in this test rather than relying on real
+	// files on disk.
 	memFs := filesys.MakeFsInMemory()
 
-	// Set up base
+	// Set up a minimal Kustomize base manifest
 	baseDir := "manifests/base"
 	require.NoError(t, memFs.MkdirAll(baseDir))
 	require.NoError(t, memFs.WriteFile(
@@ -34,7 +37,7 @@ spec:
 `),
 	))
 
-	// Updated overlay using patches
+	// Define an overlay that patches the base
 	overlayDir := "manifests/overlay"
 	require.NoError(t, memFs.MkdirAll(overlayDir))
 	require.NoError(t, memFs.WriteFile(
@@ -59,17 +62,21 @@ spec:
 `),
 	))
 
+	// --- Build and render the kubernetes api objects ---
+	// create a Kustomizer to execute the overlay
 	k := krusty.MakeKustomizer(krusty.MakeDefaultOptions())
 
+	// render all resources in the manifest
 	objs, err := deploy.RenderKustomize(memFs, k, overlayDir)
 	require.NoError(t, err)
 	require.Len(t, objs, 1, "should render exactly one object")
 
+	// validate deployment
 	u := objs[0]
 	require.Equal(t, "Deployment", u.GetKind())
 	require.Equal(t, "foo", u.GetName())
 
-	// Verify that the replicas field was patched to 3
+	// confirm that the patch changed replicas to 3
 	rep, found, err := unstructured.NestedInt64(u.Object, "spec", "replicas")
 	require.NoError(t, err)
 	require.True(t, found)
